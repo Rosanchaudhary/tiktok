1cd50afc12f29bd2e2d45aba34546002c1ca4491
Rosanchaudhary <chaudharyroshan2020@gmail.com>
2022-08-18 11:03:53 +0545

    image picker web

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,14 @@ import 'package:tiktok/views/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) {
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBCzPXNNz713b4Lbn5pk1x9myfAD1mIz-4", 
+      appId: "1:493469859666:web:2466de02940ab203bf448b",
+      messagingSenderId:  "493469859666",
+      projectId: "tiktok-49317",
+      ) 
+  ).then((value) {
     Get.put(AuthController()); 
   });
   runApp(const MyApp());
--- lib/views/screens/add_screen.dart
+++ lib/views/screens/add_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tiktok/views/screens/confirm_screen.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart' as imweb;
 
 class AddScreen extends StatefulWidget {
   const AddScreen({Key? key}) : super(key: key);
@@ -12,9 +13,8 @@ class AddScreen extends StatefulWidget {
 }
 
 class _AddScreenState extends State<AddScreen> {
-
-  
   pickvideo(ImageSource src, BuildContext context) async {
+   
     final video = await ImagePicker().pickVideo(source: src);
     if (video != null) {
       // ignore: use_build_context_synchronously
@@ -37,6 +37,7 @@ class _AddScreenState extends State<AddScreen> {
                 SimpleDialogOption(
                   onPressed: () {
                     pickvideo(ImageSource.camera, context);
+                   //imweb.ImagePickerPlugin().getVideo(source: ImageSource.camera);
                   },
                   child: Row(
                     children: const [
@@ -83,7 +84,7 @@ class _AddScreenState extends State<AddScreen> {
   @override
   void initState() {
     super.initState();
-    pickvideo(ImageSource.camera, context);
+    // pickvideo(ImageSource.camera, context);
   }
 
   @override
